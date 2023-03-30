0b17340d7709b20628e94a7c891cab54f7a5e4ec
Johan Sandberg <jvesand@gmail.com>
2023-03-30 17:11:25 +0200

    finished deployment

---
+++ Lab/production_model.py
@@ -0,0 +1,23 @@
+import os
+import joblib
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+data_path = os.path.dirname(__file__)
+
+model_final = joblib.load(data_path + "/model_final.pkl")
+scaler = joblib.load(data_path + "/scaler.bin")
+test_samples = pd.read_csv(data_path + "/test_samples.csv", index_col=0)
+
+X_test, y_test = test_samples.drop("cardio", axis=1), test_samples["cardio"]
+X_test_scaled = scaler.transform(X_test)
+
+prob0 = model_final.predict_proba(X_test_scaled)[:, 0]
+prob1 = model_final.predict_proba(X_test_scaled)[:, 1]
+pred = model_final.predict(X_test_scaled)
+
+df = pd.DataFrame(
+    {"probability class 0": prob0, "probability class 1": prob1, "prediction": pred}, index=test_samples.index
+)
+
+df.to_csv(data_path + "/prediction.csv")
