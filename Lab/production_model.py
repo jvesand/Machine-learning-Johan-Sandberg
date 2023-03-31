b72af5ce2baebede0a8dc397311dd59e55719c31
Johan Sandberg <jvesand@gmail.com>
2023-03-31 16:31:48 +0200

    cleaned up and added markdown structure and comments

--- Lab/production_model.py
+++ Lab/production_model.py
@@ -5,10 +5,12 @@ from sklearn.preprocessing import StandardScaler
 
 data_path = os.path.dirname(__file__)
 
+# load
 model_final = joblib.load(data_path + "/model_final.pkl")
 scaler = joblib.load(data_path + "/scaler.bin")
 test_samples = pd.read_csv(data_path + "/test_samples.csv", index_col=0)
 
+# make test data and scale
 X_test, y_test = test_samples.drop("cardio", axis=1), test_samples["cardio"]
 X_test_scaled = scaler.transform(X_test)
 
@@ -17,7 +19,7 @@ prob1 = model_final.predict_proba(X_test_scaled)[:, 1]
 pred = model_final.predict(X_test_scaled)
 
 df = pd.DataFrame(
-    {"probability class 0": prob0, "probability class 1": prob1, "prediction": pred}, index=test_samples.index
+    {"probability class 0": prob0, "probability class 1": prob1, "prediction": pred, "actual": y_test.values}, index=test_samples.index
 )
 
 df.to_csv(data_path + "/prediction.csv")
